df40fa8573efae039497b2d509c117ee33bcd9ab
Lasse Lauwerys <lauweryslasse@gmail.com>
2024-12-09 00:52:21 +0100

    Added some Firefox support

--- web/js/touchpadSupport.js
+++ web/js/touchpadSupport.js
@@ -1,56 +1,63 @@
 /** @type { import("../../../../web/scripts/app.js") } */
-import { app } from '../../../scripts/app.js'
-const { LGraphCanvas } = window
+import { app } from '../../../scripts/app.js';
+const { LGraphCanvas } = window;
 
-const isTouchpad = e => e.wheelDelta ? !!(e.wheelDelta % 120) : e.deltaMode === 0
-const isTouchpadZooming = e => e.ctrlKey && !!(e.deltaY % 100)
-const canTargetScroll = e => e.target.clientHeight < e.target.scrollHeight
+const isTouchpad = e => e.wheelDelta ? Boolean(e.wheelDelta % 120) : e.deltaMode === 0;
+const isTouchpadZooming = e => e.ctrlKey && Boolean(e.deltaY % 100);
+const canTargetScroll = e => e.target.clientHeight < e.target.scrollHeight;
 
 const oldProcessMouseWheel = LGraphCanvas.prototype.processMouseWheel;
 
 // Options
-const scrollZooming = true
-const touchpadZooming = false
-const zoomSpeed = 0.2
-const touchpadZoomSpeed = 1
-const allowPanningOverNonScrollableTextareas = true
-const allowZoomingOverTextareas = false
+const scrollZooming = true;
+const touchpadZooming = false;
+const zoomSpeed = 0.2;
+const touchpadZoomSpeed = 1;
+const allowPanningOverNonScrollableTextareas = true;
+const allowZoomingOverTextareas = false;
 
-let isPanning = false
+const isFirefox = "onwheel" in app.canvasEl.parentElement;
+let isPanning = false;
 
-const enablePanning = () => isPanning = true
+const enablePanning = () => isPanning = true;
 const disablePanning = () => (isPanning = false, document.removeEventListener("pointermove", disablePanning))
 
 const processMouseWheel = e => {
-  const scale = app.canvas.ds.scale
-  const touchpad = isTouchpad(e)
-  const touchpadZooming = isTouchpadZooming(e)
-  const deltaZoom =  100 / (touchpadZooming ? touchpadZoomSpeed : zoomSpeed) / scale
+  const scale = app.canvas.ds.scale;
+  const touchpad = isTouchpad(e);
+  const touchpadZooming = isTouchpadZooming(e);
+  let deltaZoom = 100 / (touchpadZooming ? touchpadZoomSpeed : zoomSpeed) / scale;
 
-  if (e.target.tagName === "TEXTAREA" && allowPanningOverNonScrollableTextareas && !canTargetScroll(e)) enablePanning()
+  if (e.target.tagName === "TEXTAREA" && allowPanningOverNonScrollableTextareas && !canTargetScroll(e)) enablePanning();
 
   if (app.canvas.graph && app.canvas.allow_dragcanvas && isPanning) {
     document.addEventListener("pointermove", disablePanning);
 
-    let { deltaX, deltaY } = e
-    if (e.shiftKey) deltaX = e.deltaY, deltaY = e.deltaX
+    let { deltaX, deltaY } = e;
+    if (e.shiftKey) {
+      deltaX = e.deltaY;
+      deltaY = e.deltaX;
+    }
 
-    if (e.metaKey || e.ctrlKey || (scrollZooming && !touchpad) || (touchpadZooming && touchpad)) {
-      if (e.metaKey) deltaZoom *= -1 / 0.5
-      app.canvas.ds.changeScale(scale - e.deltaY / deltaZoom, [e.clientX, e.clientY])
-      app.canvas.graph.change()
-    } else app.canvas.ds.mouseDrag(-deltaX, -deltaY)
+    console.log(isFirefox);
+    if ((!isFirefox && (e.metaKey || e.ctrlKey || (scrollZooming && !touchpad))) || (touchpadZooming && touchpad)) {
+      if (e.metaKey) deltaZoom *= -1 / 0.5;
+      app.canvas.ds.changeScale(scale - e.deltaY / deltaZoom, [e.clientX, e.clientY]);
+      app.canvas.graph.change();
+    } else app.canvas.ds.mouseDrag(-deltaX, -deltaY);
     
-    app.canvas.graph.change()
-    e.preventDefault()
-    return false
+    app.canvas.graph.change();
+    e.preventDefault();
+    return false;
   } else {
-    oldProcessMouseWheel.bind(app.canvas, e)
-    if (e.ctrlKey) e.preventDefault()
-    return true
+    oldProcessMouseWheel.bind(app.canvas, e);
+    if (e.ctrlKey) e.preventDefault();
+    return true;
   }
-}
+};
 
-app.canvasEl.parentElement.addEventListener("mousewheel", processMouseWheel)
+console.log(isFirefox);
 
-LGraphCanvas.prototype.processMouseWheel = e => enablePanning()
+app.canvasEl.parentElement.addEventListener(isFirefox ? "wheel" : "mousewheel", processMouseWheel);
+
+LGraphCanvas.prototype.processMouseWheel = e => enablePanning();
